08ebb6ec82b2d74664c312311ab8f52b8fd37e9c
Chad Looker <chad.looker@gmail.com>
2020-03-09 17:56:51 -0400

    Add basic vert.x stuff

---
+++ src/main/java/com/clooker/vertx/Item.java
@@ -0,0 +1,22 @@
+package com.clooker.vertx;
+
+public class Item {
+  private String description;
+  private String name;
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
