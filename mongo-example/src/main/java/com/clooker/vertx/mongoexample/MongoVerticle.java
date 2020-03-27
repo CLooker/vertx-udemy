1dd83603525fec641278eb2f769388a373c0a7e7
Chad Looker <chad.looker@gmail.com>
2020-03-27 10:21:20 -0400

    Add clustering/messaging

--- mongo-example/src/main/java/com/clooker/vertx/mongoexample/MongoVerticle.java
+++ mongo-example/src/main/java/com/clooker/vertx/mongoexample/MongoVerticle.java
@@ -2,6 +2,7 @@ package com.clooker.vertx.mongoexample;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
@@ -17,14 +18,19 @@ public class MongoVerticle extends AbstractVerticle {
   private static MongoClient mongoClient = null;
 
   public static void main(String[] args) {
-    Vertx vertx = Vertx.vertx();
-    mongoClient = MongoClient.createShared(
-        vertx,
-        new JsonObject()
-            .put("connection_string", "mongodb://localhost:27017/testDB")
-            .put("useObjectId", true)
-    );
-    vertx.deployVerticle(new MongoVerticle());
+    VertxOptions vertxOptions = new VertxOptions().setClustered(true);
+    Vertx.clusteredVertx(vertxOptions, results -> {
+      if (results.succeeded()) {
+        Vertx vertx = results.result();
+        vertx.deployVerticle(new MongoVerticle());
+        mongoClient = MongoClient.createShared(
+            vertx,
+            new JsonObject()
+                .put("connection_string", "mongodb://localhost:27017/testDB")
+                .put("useObjectId", true)
+        );
+      }
+    });
   }
 
   @Override
@@ -33,6 +39,26 @@ public class MongoVerticle extends AbstractVerticle {
     Router router = Router.router(vertx);
     router.get("/products").handler(this::getProducts);
     vertx.createHttpServer().requestHandler(router::accept).listen(3000);
+    vertx.eventBus().consumer("com.clooker.vertx.messaging", message -> {
+      System.out.println("message when received: " + message.body());
+      JsonObject replyMessage = new JsonObject()
+          .put("responseCode", "OK")
+          .put("message", "This is a reply to " + message.body());
+      System.out.println("replyMessage when sent: " + replyMessage);
+      message.reply(replyMessage);
+    });
+
+    vertx.setTimer(2000, handler -> {
+      JsonObject obj = new JsonObject().put("info", "Hi");
+      String message = obj.toString();
+      System.out.println("message when sent: " + message);
+      vertx.eventBus().send("com.clooker.vertx.messaging", message, reply -> {
+        if (reply.succeeded()) {
+          String replyMessage = reply.result().body().toString();
+          System.out.println("replyMessage when received: " + replyMessage);
+        }
+      });
+    });
   }
 
   private void getProducts(RoutingContext routingContext) {
